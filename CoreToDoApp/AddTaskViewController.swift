34400ba90ef2cbc904a668b9c6b8c9760970f2ea
Admin <admin@admins-macbook-pro-2.local>
2017-09-07 14:13:35 +0300

    // first COMMIT //  added navigation controller + another view controller + new cocoa file + bar button item +  segue

---
+++ CoreToDoApp/AddTaskViewController.swift
@@ -0,0 +1,21 @@
+//
+//  AddTaskViewController.swift
+//  CoreToDoApp
+//
+//  Created by Admin on 07/09/2017.
+//  Copyright © 2017 home. All rights reserved.
+//
+
+import UIKit
+
+class AddTaskViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+  
+
+}
